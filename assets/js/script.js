358edb2309c64fe0ef1c8ee74cb5a8ad252d02cb
hazratzn <hazratzn@code.edu.az>
2022-02-12 06:52:15 -0800

    Second Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,41 @@
-$(function () {
-
+// $(function(){
 
 
 
     
+// })
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+var clock = $('.clock').FlipClock({
+    clockFace: 'DailyCounter',
+    autoStart: false,
+    callbacks: {
+      stop: function() {
+        $('.message').html('The clock has stopped!')
+      }
+    }
+    
 });
+// set time
+clock.setTime(220880);
+
+// countdown mode
+clock.setCountdown(true);
+
+// start the clock
+clock.start();
